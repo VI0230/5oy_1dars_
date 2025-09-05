7064aa9e811f3c86b9f89b1cca0cfa45e2f41386
VI0230 <jeonshi2321@gmail.com>
2025-09-05 23:27:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var ism = prompt("Ismingizni kiriting ");
+var familya = prompt(" Familyangizni kiriting")
+var yosh = prompt ("Yoshingizni kiriting")
+var fan = prompt ("Yoqtirgan faningizni kiriting")
+var manzil = prompt ("Yashaydigan tumaningizni kiriting")
+let result = confirm("Ma'lumotlaringizni kiritganingiz uchun tashakkur!");
+console.log(ism,familya,yosh,fan,manzil);
+
+console.log("Ism: " + ism);
+console.log("familya: " + familya);
+console.log("Yosh: " + yosh);
+console.log("fan: " + fan);
+console.log("manzil: " + manzil);
